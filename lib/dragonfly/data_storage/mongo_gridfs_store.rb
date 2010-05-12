d3ef13d9b30ba07e66cad24222513bc5db6e70ff
Kevin Evans <kwevans@gmail.com>
2010-05-12 16:52:14 +0100

    make DB a config option

--- lib/dragonfly/data_storage/mongo_gridfs_store.rb
+++ lib/dragonfly/data_storage/mongo_gridfs_store.rb
@@ -5,29 +5,29 @@ module Dragonfly
 
     class MongoGridfsStore < Base
       
-#      include Configurable
+      include Configurable
       
-#      configurable_attr :grid_fs_database
+      configurable_attr :grid_fs_database
       
       def store(temp_object)
 
-        db = Mongo::Connection.new.db(Mogoid.database)
+        db = Mongo::Connection.new.db(grid_fs_database)
         grid = Mongo::Grid.new(db)
 
         # returns object id
-        id = grid.put(File.read(temp_object.path), :filename=>'something.jpg')
+        id = grid.put(File.read(temp_object.path))
         id
       end
 
       def retrieve(uid)
-        db = Mongo::Connection.new.db(Mongoid.database)
+        db = Mongo::Connection.new.db(grid_fs_database)
         grid = Mongo::Grid.new(db)
         file = grid.get(Mongo::ObjectID.from_string(uid))
         file.read
       end
       
       def destroy(uid)
-        db = Mongo::Connection.new.db(Mongoid.database)
+        db = Mongo::Connection.new.db(grid_fs_database)
         grid = Mongo::Grid.new(db)
         grid.delete(Mongo::ObjectID.from_string(uid))
       end
